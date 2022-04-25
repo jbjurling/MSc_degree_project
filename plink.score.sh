5e812809e843984d2e1be479e456416a4d0191e1
jbjurling <bjurlingjosefin@gmail.com>
2022-04-25 22:37:36 +0200

    Update plink.score.sh

--- plink.score.sh
+++ plink.score.sh
@@ -1,14 +1,14 @@
-#! /bin/bash -l
+#! /bin/bash -l 
 #SBATCH -A sens2017538
-#SBATCH -p core -n 10
+#SBATCH -p core -n 8
 #SBATCH -t 48:00:00
 
-module load bioinfo-tools
-module load plink
+genotype=/proj/sens2017538/nobackup/UKBB_IMP_DOSAGE_V3_bim_bed
+path=/proj/sens2017538/nobackup/Exjobb/Josefin/PRS/LDpred2/breast_cancer #ovarian_cancer for OC
+out=/proj/sens2017538/nobackup/Exjobb/Josefin/PRS/LDpred2/breast_cancer/PRS_score #ovarian_cancer for OC
 
-path=/proj/sens2017538/nobackup/Exjobb/Josefin/PRS/LDpred2/breast_cancer #/ovarian_cancer for OC
-genotypes=/proj/sens2017538/nobackup/UKBB_IMP_DOSAGE_V3_bim_bed
-out=/proj/sens2017538/nobackup/Exjobb/Josefin/PRS/LDpred2/breast_cancer/PRS_score #/ovarian_cancer/PRS_score for OC
+module load bioinfo-tools
+module load plink2
 
 cd $out
 
@@ -16,10 +16,10 @@ for chr in {1..22}
 
 do
 
-plink --bed $genotypes/chr${chr}.bed \
---bim $genotypes/chr${chr}.dedup.bim \
---fam $path/ukb41143.BC.fam \ #/ukb41143.OC.fam for OC
---score $path/beta_grid.score 1 2 ${score} header sum \
---out PRS_score_${chr}_${score}
+plink2 --pgen $genotype/chr${chr}.pgen \
+--psam $path/ukb41143.BC.fam \ #.OC. for OC
+--pvar $genotype/chr${chr}.dedup.bim \
+--score $path/beta_grid.score.a1 1 2 ${score} header cols=+scoresums \
+--out model_${score}_chr${chr}
 
 done
